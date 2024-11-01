d01455d61516d2760836f54e5ff42c6b4408eda4
ACCBDD <9992251+ACCBDD@users.noreply.github.com>
2024-11-01 15:53:21 -0700

    initial port to 1.21.1

--- src/main/java/com/accbdd/farmers_croptopia/FarmersCroptopia.java
+++ src/main/java/com/accbdd/farmers_croptopia/FarmersCroptopia.java
@@ -1,11 +1,9 @@
 package com.accbdd.farmers_croptopia;
 
 import com.mojang.logging.LogUtils;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
 import org.slf4j.Logger;
 
 @Mod(FarmersCroptopia.MODID)
@@ -14,14 +12,8 @@ public class FarmersCroptopia
     public static final String MODID = "farmers_croptopia";
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public FarmersCroptopia()
+    public FarmersCroptopia(IEventBus modEventBus, ModContainer modContainer)
     {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    private void commonSetup(final FMLCommonSetupEvent event)
-    {
-
+        //NeoForge.EVENT_BUS.register(this);
     }
 }
