c8ac8fac1999186590e2cea365025a401cf3db7f
ACCBDD <9992251+ACCBDD@users.noreply.github.com>
2024-11-05 09:28:22 -0800

    update fd recipes to new 1.21 format

--- generator/generatejsons.py
+++ generator/generatejsons.py
@@ -19,56 +19,72 @@ plants = [("artichoke", "artichokes"), ("asparagus", "asparagus"), ("barley", "b
 input_files = glob.glob('input/*', recursive=True)
 
 # seeds from plants - cutting
-for plant, plural in plants:
-    seed_recipe = {
-        "type": "farmersdelight:cutting",
-        "ingredients": [
-            {
-                "item": "croptopia:" + plant
-            }
-        ],
-        "result": [
-            {
-                "item": "croptopia:" + plant + "_seed"
-            },
-            {
-                "chance": 0.5,
-                "count": 1,
-                "item": "croptopia:" + plant + "_seed"
-            }
-        ],
-        "tool": {
-            "tag": "forge:tools/knives"
-        }
-    }
-    with open("output/" + plant + "_seed.json", "w") as f:
-        json.dump(seed_recipe, f, ensure_ascii=False, indent=2)
+# for plant, plural in plants:
+#     seed_recipe = {
+#         "type": "farmersdelight:cutting",
+#         "ingredients": [
+#             {
+#                 "item": "croptopia:" + plant
+#             }
+#         ],
+#         "result": [
+#             {
+#                 "item": "croptopia:" + plant + "_seed"
+#             },
+#             {
+#                 "chance": 0.5,
+#                 "count": 1,
+#                 "item": "croptopia:" + plant + "_seed"
+#             }
+#         ],
+#         "tool": {
+#             "tag": "forge:tools/knives"
+#         }
+#     }
+#     with open("output/" + plant + "_seed.json", "w") as f:
+#         json.dump(seed_recipe, f, ensure_ascii=False, indent=2)
 
 # convert croptopia cooking pot recipes to fd pot recipes
-converted_count = 0
+# converted_count = 0
+# for file in input_files:
+#     with open(file, 'r') as f:
+#         json_file = json.load(f)
+#         if json_file['type'] == 'minecraft:crafting_shapeless':
+#             if {'item': 'croptopia:cooking_pot'} in json_file['ingredients']:
+#                 if {'item': 'croptopia:food_press'} not in json_file['ingredients']:
+#                     json_file['ingredients'].remove({'item': 'croptopia:cooking_pot'})
+#                     converted_recipe = {
+#                         "type": "farmersdelight:cooking",
+#                         "cookingtime": 200,
+#                         "experience": 0.8,
+#                         "ingredients": json_file['ingredients'],
+#                         "result": json_file['result']
+#                     }
+#                     with open('output/' + file[6:], 'w') as g:
+#                         json.dump(converted_recipe, g, ensure_ascii=False, indent=2)
+#                     converted_count += 1
+#                 else:
+#                     print('found hybrid: ' + file)
+#         elif json_file['type'] == 'minecraft:crafting_shaped':
+#             if {'item': 'croptopia:cooking_pot'} in json_file['key'].values():
+#                 print('cooking recipe ' + file + ' is shaped, needs manual conversion')
+# print('converted ' + str(converted_count) + ' out of ' + str(len(input_files)) + ' input files')
+
+# convert fd cooking to croptopia cooking pot recipes
 for file in input_files:
     with open(file, 'r') as f:
         json_file = json.load(f)
-        if json_file['type'] == 'minecraft:crafting_shapeless':
-            if {'item': 'croptopia:cooking_pot'} in json_file['ingredients']:
-                if {'item': 'croptopia:food_press'} not in json_file['ingredients']:
-                    json_file['ingredients'].remove({'item': 'croptopia:cooking_pot'})
-                    converted_recipe = {
-                        "type": "farmersdelight:cooking",
-                        "cookingtime": 200,
-                        "experience": 0.8,
-                        "ingredients": json_file['ingredients'],
-                        "result": json_file['result']
-                    }
-                    with open('output/' + file[6:], 'w') as g:
-                        json.dump(converted_recipe, g, ensure_ascii=False, indent=2)
-                    converted_count += 1
-                else:
-                    print('found hybrid: ' + file)
-        elif json_file['type'] == 'minecraft:crafting_shaped':
-            if {'item': 'croptopia:cooking_pot'} in json_file['key'].values():
-                print('cooking recipe ' + file + ' is shaped, needs manual conversion')
-print('converted ' + str(converted_count) + ' out of ' + str(len(input_files)) + ' input files')
+        if json_file['type'] == "farmersdelight:cooking":
+            json_file['ingredients'].append({'item': 'croptopia:cooking_pot'})
+            json_file['ingredients'].append({'item': 'minecraft:bowl'})
+            converted_recipe = {
+                "type": "minecraft:crafting_shapeless",
+                "ingredients": json_file['ingredients'],
+                "category": "misc",
+                "result": json_file['result']
+            }
+            with open('output/' + file[6:], 'w') as g:
+                json.dump(converted_recipe, g, ensure_ascii=False, indent=2)
 
 # create advancements for all recipes
 for file in input_files:
@@ -97,5 +113,5 @@ for file in input_files:
                 ]
             ]
         }
-        with open('output/' + file[6:], 'w') as g:
+        with open('output/advancements/' + file[6:], 'w') as g:
             json.dump(advancement_json, g, ensure_ascii=False, indent=2)
